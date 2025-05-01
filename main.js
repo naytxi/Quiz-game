373384627511d60f2d50e28435a425c748616789
Nahia Otegui <nay.fullstack@gmail.com>
2025-05-01 18:31:49 +0200

    aparecen las preguntas

--- main.js
+++ main.js
@@ -17,29 +17,44 @@ async function startGame() {
   setNextQuestion();
 }
 
-//Creamos una funcion asincrona que se encargara de obtener las preguntas de la API, formatearlas y devolverlas
+//Creamos una funcion asincrona que se encargara de obtener las preguntas y sus respuestas de la API con el token generado, formatearlas y devolverlas
 async function getQuestions() {
-  const res = await fetch('https://opentdb.com/api.php?amount=10&type=multiple');
+  const res = await fetch('https://quizapi.io/api/v1/questions?limit=10', {
+    headers: {
+      'X-Api-Key': 'VqCrxdX2mo4HsoTysxcRjIi9qFIgh52OqSvn8lEs'
+    }
+  });
+
   const data = await res.json();
 
-  return data.results.map(q => {
-    const answers = [...q.incorrect_answers.map(a => ({
-      text: a,
-      correct: false
-    })), { text: q.correct_answer, correct: true }];
+  return data.map(q => {
+    const answers = [];
+
+    for (let key in q.answers) {
+      if (q.answers[key]) {
+        const letter = key.split('_')[1]; 
+        const correct = q.correct_answers[`answer_${letter}_correct`] === 'true';
+        answers.push({
+          text: q.answers[key],
+          correct: correct
+        });
+      }
+    }
 
     return {
-      question: decodeHTML(q.question),
-      answers: shuffle(answers)
+      question: q.question,
+      answers: answers
     };
   });
 }
 
+//Creamos una funcion que se encargara de mostrar la siguiente pregunta, reiniciar el estado y mostrar la pregunta actual Y sus respuestas
 function setNextQuestion() {
   resetState();
   showQuestion(questionList[currentQuestionIndex]);
 }
 
+//Esta funcion se encargara de mostrar la pregunta y sus respuestas en el DOM, creando un boton por cada respuesta y añadiendo un evento click a cada uno de ellos
 function showQuestion(question) {
   questionElement.innerText = question.question;
   question.answers.forEach(answer => {
@@ -52,19 +67,20 @@ function showQuestion(question) {
   });
 }
 
+//Esta en cambio se encargara de reiniciar el estado de las preguntas, ocultar el boton de siguiente y limpiar las respuestas anteriores
 function resetState() {
   clearStatusClass(document.body);
   nextButton.classList.add('hide');
   answerButtonsElement.innerHTML = '';
 }
 
+//Aqui se encargara de seleccionar la respuesta correcta o incorrecta, deshabilitar los botones y mostrar el boton de siguiente si no es la ultima pregunta
 function selectAnswer(e) {
   const selectedButton = e.target;
   const correct = selectedButton.dataset.correct;
 
   setStatusClass(selectedButton, correct);
 
-  // Deshabilitar todos los botones
   Array.from(answerButtonsElement.children).forEach(button => {
     button.disabled = true;
     setStatusClass(button, button.dataset.correct);
@@ -78,6 +94,7 @@ function selectAnswer(e) {
   }
 }
 
+//Esta funcion se encargara de añadir la clase correcta o incorrecta a los botones dependiendo de si la respuesta es correcta o no
 function setStatusClass(element, correct) {
   clearStatusClass(element);
   if (correct) {
@@ -86,22 +103,20 @@ function setStatusClass(element, correct) {
     element.classList.add('color-wrong');
   }
 }
-
+//Esta funcion se encargara de limpiar el estado de los botones, eliminando las clases correctas o incorrectas
 function clearStatusClass(element) {
   element.classList.remove('color-correct');
   element.classList.remove('color-wrong');
 }
 
-function shuffle(array) {
-  return array.sort(() => Math.random() - 0.5);
-}
-
+//Aqui decodificamos el HTML de las respuestas, para que se muestre correctamente en el DOM
 function decodeHTML(html) {
   const txt = document.createElement("textarea");
   txt.innerHTML = html;
   return txt.value;
 }
 
+//Añadimos los eventos
 startButton.addEventListener('click', startGame);
 
 nextButton.addEventListener('click', () => {
